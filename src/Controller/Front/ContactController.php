1f37d82868f0ee94d66b7aa2e5510890a72f75bd
github_pierreFerre <github_pedr1ferre@gmail.com>
2021-05-18 14:34:07 +0200

    CREATE: contact controller and form

---
+++ src/Controller/Front/ContactController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/front/contact", name="front_contact")
+     */
+    public function index(): Response
+    {
+        return $this->render('front/contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+        ]);
+    }
+}
--- src/Controller/Front/MainController.php
+++ src/Controller/Front/MainController.php
@@ -2,19 +2,45 @@
 
 namespace App\Controller\Front;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Portfolio;
+use App\Repository\BiographyRepository;
+use App\Repository\PortfolioRepository;
+use App\Repository\PhotographyRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="front_main")
+     * @Route("/", name="front_home")
      */
-    public function index(): Response
+    public function home(PortfolioRepository $portfolioRepository): Response
     {
-        return $this->render('front/portfolio/index.html.twig', [
-            'controller_name' => 'PortfolioController',
+        return $this->render('front/main/index.html.twig', [
+            'portfolios' => $portfolioRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/collection/{id<\d+>}", name="front_portfolio_read")
+     */
+    public function portfolio(Portfolio $portfolio = null, PhotographyRepository $photographyRepository)
+    {
+        $photographies = $photographyRepository->findBy(['portfolio' => $portfolio]);
+        return $this->render('front/main/collection.html.twig', [
+            'portfolio' => $portfolio,
+            'photographies' => $photographies,
+        ]);
+    }
+
+    /**
+     * @Route("/biographie", name="front_biography_read")
+     */
+    public function biography(BiographyRepository $biographyRepository)
+    {
+        return $this->render('front/main/biography.html.twig', [
+            'biographies' => $biographyRepository->findAll(),
         ]);
     }
 }
